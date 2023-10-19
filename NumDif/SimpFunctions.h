a329697d48e53f691119751006dac6fca403b65a
StanislavaVVV <91430313+StanislavaVVV@users.noreply.github.com>
2023-10-19 18:12:02 +0300

    add Численное дифференциирование
    Версия численного дифференциирования от 19 октября

---
+++ NumDif/SimpFunctions.h
@@ -0,0 +1,96 @@
+#include <vector>
+#include <string>
+#include <cmath>
+using namespace std;
+
+vector<double> GaussSolver(double** a, double* y, int n)
+{
+    double max;
+    int k, index;
+    const double eps = std::numeric_limits<double>::min();  //Точность
+    vector<double> x;
+    k = 0;
+    for (; k < n; k++)
+    {
+        //Поиск строки с максимальным a[i][k]
+        max = abs(a[k][k]);
+        index = k;
+        for (int i = k + 1; i < n; i++)
+        {
+            if (abs(a[i][k]) > max)
+            {
+                max = abs(a[i][k]);
+                index = i;
+            }
+        }
+
+        //Переносим строку с максимальным a[i][k] в самый верх
+        for (int j = 0; j < n; j++)
+        {
+            double temp = a[k][j];
+            a[k][j] = a[index][j];
+            a[index][j] = temp;
+        }
+
+        //Также переносим соответствующий этой строке элемент в столбце решений наверх
+        double temp = y[k];
+        y[k] = y[index];
+        y[index] = temp;
+
+        // Нормализация уравнений
+        for (int i = k; i < n; i++)
+        {
+            double temp = a[i][k];
+            if (abs(temp) < eps) continue; //Для нулевого коэффициента пропустить
+            for (int j = 0; j < n; j++)
+                a[i][j] = a[i][j] / temp; //Делим всю строку на максимальный элемент
+            y[i] = y[i] / temp;
+            if (i == k)  continue; //Уравнение не вычитать само из себя
+            for (int j = 0; j < n; j++)
+                a[i][j] = a[i][j] - a[k][j]; //Вычитаем верхнюю строку из всех нижних
+            y[i] = y[i] - y[k];
+        }
+    }
+    //Обратная подстановка (имеем верхнетреугольную матрицу, начинаем искать x(i) с нижней строки, двигаясь наверх
+    for (k = n - 1; k >= 0; k--)
+    {
+        x.insert(x.begin(), y[k]);
+        for (int i = 0; i < k; i++)
+            y[i] = y[i] - a[i][k] * y[k];
+    }
+    return x;
+}
+
+vector<double> GetSolution(const vector<double>& points) { //nodiscard указывает, что возвращаемое функцией значение нельзя игнорировать и нужно сохранить в какую-либо переменную
+
+    int n = points.size();
+    
+    double** A = new double* [n];// выделяем место под матрицу
+
+    //Заполняем матрицу по схеме
+    for (int i = 0; i < n; i++) {
+        A[i] = new double[n];
+        for (int j = 0; j < n; j++) {
+            A[i][j] = pow(points[j], i);
+        }
+    }
+
+    //Здесь лежат решения - для производной первого порядка = 1, остальные = 0
+    double* y = new double[n];
+    for (int i = 0; i < n; i++) {
+        y[i] = 0;
+    };
+    y[1] = 1;
+
+    //Метод Гаусса решения системы алгебраических уравн.
+    vector<double> x = GaussSolver(A, y, n);
+
+    delete[] y;
+
+    for (int i = 0; i < n; i++) {
+        delete[] A[i];
+    };
+    delete[] A;
+
+    return (x);
+}
---
+++ NumDif/main.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include "SimpFunctions.h"
+
+using namespace std;
+
+double func(double x) { // Это наша функция - в данном случае exp^x
+    return exp(x);
+}
+
+int main(){
+    double x0 = 1;
+    double h = 0.01;
+    int N = 2;
+    vector<double> points(N);
+
+    // Сделаем шаблон {... x0-2h, x0-h, x0, x0+h, x0 + 2h,...}
+    for (int i = 0; i < N; i++){
+        points[i] = x0 + h * (-N/2 + i);
+        //cout << points[i] << endl;
+    }
+
+    vector<double> A = GetSolution(points);
+
+    int CentCoefNum = 0;
+    for(int i = 0; i < A.size(); i++){
+        if (points[i] == x0){
+            cout << "Central coef = " << A[i] << endl;
+            CentCoefNum = i;
+            break;
+        }
+    }
+
+    cout << "Other coef = ";
+    for (int i = 0; i < A.size(); i++){
+        if (i == A.size()  - 1) break;
+        if(i != CentCoefNum) cout << A[i] / h << ", ";
+    }
+    cout << A[A.size()-1] / h << endl;
+
+    // производная для e^x в точке x0
+    // считаем точки на шаблоне
+
+    double dfdx = 0; //Производная
+    for (int i = 0; i < points.size(); i++) {
+        dfdx += func(x0 + points[i] * h) * A[i] / h;
+    }
+    cout << "df/dx = " << dfdx << endl;
+
+
+    // построим график зависимости логарифма ошибки от логарифма шага
+    ofstream err, lnh;
+
+    // окрываем файлы для записи
+    err.open("err.txt"); 
+    lnh.open("lnh.txt");
+
+    double real_dfdx = exp(x0);  //реальное значение производной в точке x0
+    double h_iteration = 0;
+    if (err.is_open() and lnh.is_open())
+    {
+
+        for(int i = 0; i < 1600; i++){ //16 это максимальные кол-во нулей после запятой в значении h
+
+            h_iteration = 1 * pow(10,-i/100);
+
+            lnh << h_iteration << endl;
+
+            dfdx = 0;
+            for (int j = 0; j < points.size(); j++) {
+                dfdx += func(x0 + points[j] * h_iteration) * A[j] / h_iteration;
+            }
+            err << abs(real_dfdx - dfdx) << endl;
+        }
+
+    }
+    err.close();
+    lnh.close();
+
+    return 0;
+}
